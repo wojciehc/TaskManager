923794709a7195d4d899549df8cfcfce3d0a1e71
Wojciech Wilk <wojtek.wilk2@gmail.com>
2022-04-13 12:29:36 +0200

    Added views, views models and swiching between views

---
+++ TaskManager/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.ViewModel 
+{
+    class MainViewModel :  ObservableObject
+    {
+
+        public RelayCommand TasksViewCommand { get; set; }
+
+        public RelayCommand AddTaskViewCommand { get; set; }
+
+        public TasksViewModel TasksVM { get; set; }
+
+        public AddTaskViewModel AddTaskVM { get; set; } 
+
+        private object _currentView;
+
+        public object CurrentView 
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        {
+            TasksVM = new TasksViewModel();
+            AddTaskVM = new AddTaskViewModel();
+
+            CurrentView = TasksVM;
+
+            TasksViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TasksVM;
+            });
+
+            AddTaskViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = AddTaskVM;
+            });
+
+        }
+
+    }
+}
---
+++ TaskManager/ViewModel/TasksViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.ViewModel
+{
+    internal class TasksViewModel : ObservableObject
+    {
+        public TasksViewModel() 
+        {
+
+        }
+    }
+}
