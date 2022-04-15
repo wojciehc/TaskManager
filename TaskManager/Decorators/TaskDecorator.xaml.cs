e5577df9ce7c7284a83f5acf8fde628128884fde
gmtrk <gmtrkk@gmail.com>
2022-04-15 02:01:15 +0200

    added bordertheme (task highlights), (probowalem przez godzine ogarnac jak to ogarnac w templacie, dowiedzialem sie cos o dekoratorach ale nie rozumier ich za bardzo, obawiam sie ze bedziemy musieli zmienic bordery na jakas inna kontrolke, np przycisk nie bylby glupi)

---
+++ TaskManager/Decorators/TaskDecorator.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManager.Decorators
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            TextBlock taskName = new TextBlock();
+            TextBlock taskDescription = new TextBlock();
+            TextBlock startDate = new TextBlock();
+            TextBlock endDate = new TextBlock();
+            taskName.Text = "namenameTask";
+            taskDescription.Text = "descdescTask";
+            startDate.Text = "24.03.2013";
+            endDate.Text = "27.09.2023";
+        }
+    }
+}
